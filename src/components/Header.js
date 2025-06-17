7e5be40352bbf27cb2457cc281d93a258226498f
taehun.kim <xognswldk@gmail.com>
2025-06-17 14:50:46 +0900

    ✨ feat: 헤더 컴포넌트 상세 구현 및 메인 페이지 적용
    - ✨ Header.js: 글로벌 내비게이션 바 구조 및 기능 상세 구현
    - 🎨 ProductListPage.js: Header 컴포넌트 적용
    - 💄 index.css: Header 스타일 구현 (로고, 검색창, 버튼 정렬 등)
    - 🎨 reset.css: 기본 스타일 초기화
    - 🚧 userApi.js: API 호출 임시 비활성화 처리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,34 @@
-export default function Header() {
+export default function createHeader() {
   const header = document.createElement('header');
   header.className = 'gnb';
 
   header.innerHTML = `
-    <div class="gnb-inner">
-      <h1 class="logo">
-        <a href="#/">
-          <img src="../assets/images/Logo-hodu.png" alt="HODU 로고" class="logo-img" />
-        </a>
-      </h1>
-      <div class="gnb-search">
-        <input type="text" placeholder="상품을 검색해보세요" disabled />
-        <button disabled>
-          <img src="../assets/images/icon-search.svg" alt="검색" />
+    <div class="gnb-wrap">
+      <div class="gnb-left">
+        <h1 class="logo">
+          <a href="#/">
+            <img class="logo-img" src="../src/assets/images/Logo-hodu.png" alt="HODU">
+          </a>
+        </h1>
+        <form class="gnb-search" role="search">
+        <input type="search" placeholder="상품을 검색해보세요" disabled />
+        <button type="submit" disabled>
+          <img src="../src/assets/images/icon-search.svg" alt="검색" />
         </button>
+      </form>
       </div>
-      <nav class="gnb-btns">
+      <div class="gnb-right">
+        <nav class="gnb-btns"  aria-label="사용자 메뉴">
         <button class="btn-cart">
-          <img src="../assets/images/icon-shopping-cart.svg" alt="장바구니" />
+          <img src="../src/assets/images/icon-shopping-cart.svg" alt="" />
+          장바구니
         </button>
         <button class="btn-mypage">
-          <img src="../assets/images/icon-user.svg" alt="마이페이지" />
+          <img src="../src/assets/images/icon-user.svg" alt="" />
+          마이페이지
         </button>
       </nav>
+      </div>
     </div>
   `;
 
