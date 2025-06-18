f036903208661a3e7580e2d636fd0d881b011c9f
taehun.kim <xognswldk@gmail.com>
2025-06-18 14:26:30 +0900

    🔧 chore: 해시 변경 및 초기 로딩 시 라우터 실행되도록 이벤트 연결 추가

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@ import router from './router/router.js';
 document.addEventListener('DOMContentLoaded' ,() => {
   router();
 })
+window.addEventListener('hashchange', router);      // 주소가 바뀔 때마다 실행
+window.addEventListener('DOMContentLoaded', router); // 페이지 처음 로딩 시 실행
